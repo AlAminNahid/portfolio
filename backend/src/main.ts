ccc2698a5aabc13bd459829b16f4f5b0e245a308
AlAminNahid <robin.nahid123@gmail.com>
2026-03-11 16:48:06 +0600

    updated cores url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,10 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
-    origin: ['http://localhost:4001', 'portfolio-six-murex-64.vercel.app'],
+    origin: [
+      'http://localhost:4001',
+      'https://portfolio-six-murex-64.vercel.app',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });

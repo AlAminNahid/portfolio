94facaf336c1ea7453f7c31d4ce93e8e0c8b699b
AlAminNahid <robin.nahid123@gmail.com>
2026-03-11 19:31:58 +0600

    added the url to the .env

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
-    origin: ['http://localhost:4001', 'portfolio-six-murex-64.vercel.app'],
+    origin: [process.env.FRONTEND_LOCAL_URL, process.env.FRONTEND_URL],
     credentials: true,
   });
 
